3000479d411d6f76f2601d450b522b445232aac0
Adam Kloboucnik <adam@klob.cz>
2021-03-05 13:57:23 +0100

    OverlineMessage can be constructed out of a tuple and split to tuple
    - tuple is  (hash, rest of message)

--- src/overline.rs
+++ src/overline.rs
@@ -4,11 +4,13 @@
 //! physical LoRa message. Defines utility struct [OverlineMessageStore] which enabled
 //! implementation of overline message retransmission rules
 use heapless::{consts::*, FnvIndexMap, FnvIndexSet, Vec};
-use typenum::Unsigned;
+use typenum::{op, Unsigned, *};
 
 pub type MaxLoraPayloadLength = U255;
 pub type OverlineMessageHashLength = U16;
+pub type OverlineMessageMaxDataLength = op!(MaxLoraPayloadLength - OverlineMessageHashLength);
 pub type OverlineMessageData = Vec<u8, MaxLoraPayloadLength>;
+pub type OverlineMessageDataPart = Vec<u8, OverlineMessageHashLength>; // FIXME better naming
 pub type OverlineMessageHash = Vec<u8, OverlineMessageHashLength>;
 
 #[derive(Debug, PartialEq)]
@@ -33,12 +35,44 @@ pub enum OverlineMessageType {
 pub struct OverlineMessage(pub OverlineMessageData);
 
 impl OverlineMessage {
+    pub fn try_from_hash_data(
+        hash: OverlineMessageHash,
+        data: OverlineMessageDataPart,
+    ) -> Result<Self, Error> {
+        if hash.len() + data.len() > MaxLoraPayloadLength::USIZE {
+            return Err(Error::InvalidOverlineMessage);
+        }
+
+        let mut vec = OverlineMessageData::new();
+        vec.extend_from_slice(&hash[0..])
+            .map_err(|_| Error::InvalidOverlineMessage)?;
+        vec.extend_from_slice(&data[0..])
+            .map_err(|_| Error::InvalidOverlineMessage)?;
+
+        Ok(OverlineMessage(vec))
+    }
+
+    pub fn into_hash_data_tuple(
+        self,
+    ) -> Result<(OverlineMessageHash, OverlineMessageDataPart), Error> {
+        let hash = self.hash()?;
+        let data_part = self.data_part()?;
+        Ok((hash, data_part))
+    }
+
     pub fn hash(&self) -> Result<OverlineMessageHash, Error> {
         if self.0.len() < OverlineMessageHashLength::USIZE {
             return Err(Error::InvalidOverlineMessage);
         }
 
         match OverlineMessageHash::from_slice(&self.0[0..OverlineMessageHashLength::USIZE]) {
+            Ok(h) => Ok(h),
+            Err(()) => Err(Error::InvalidOverlineMessage),
+        }
+    }
+
+    pub fn data_part(&self) -> Result<OverlineMessageDataPart, Error> {
+        match OverlineMessageDataPart::from_slice(&self.0[OverlineMessageHashLength::USIZE..]) {
             Ok(h) => Ok(h),
             Err(()) => Err(Error::InvalidOverlineMessage),
         }
@@ -69,6 +103,7 @@ pub enum OverlineStoreRecvOutcome {
 
 /// Store is responsible for applying rules for storing and possible retransmission of overline
 /// messages seen by the node
+#[derive(Default)]
 pub struct OverlineMessageStore {
     /// one tick duration in ms, used for deciding expiration in [`Self::tick_try_send`]
     tick_duration: u32,
@@ -77,6 +112,10 @@ pub struct OverlineMessageStore {
 }
 
 impl OverlineMessageStore {
+    pub fn new() -> Self {
+        OverlineMessageStore::default()
+    }
+
     /// used when node received a message
     pub fn recv(&mut self, message: OverlineMessage) -> Result<OverlineStoreRecvOutcome, ()> {
         let hash = message.hash().unwrap();
