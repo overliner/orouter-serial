b13b12f5d3e5a133128428eff92c1249f0e2166c
Adam Kloboucnik <adam@klob.cz>
2024-01-05 16:41:52 +0100

    Fix examples to use the new API

--- examples/read.rs
+++ examples/read.rs
@@ -1,11 +1,7 @@
 use std::io;
 use std::time::Duration;
 
-use orouter_serial::host::{
-    calculate_cobs_overhead,
-    codec::{UsbCodec, MAX_USB_FRAME_LENGTH},
-    Message, MessageReader, StatusCode, RAWIQ_DATA_LENGTH,
-};
+use orouter_serial::message;
 
 const PORT_NAME: &'static str = "/dev/ttyUSB0";
 
@@ -14,21 +10,20 @@ fn main() {
         .timeout(Duration::from_millis(10))
         .open();
 
-    let mut message_reader =
-        MessageReader::<{ calculate_cobs_overhead(RAWIQ_DATA_LENGTH) }, 17>::default(); // ceil(4116 / 256)
-
     match port {
         Ok(mut port) => {
             let mut serial_buf: Vec<u8> = vec![0; 1000];
             loop {
                 match port.read(serial_buf.as_mut_slice()) {
                     Ok(t) => {
-                        for chunk in serial_buf[..t].chunks(MAX_USB_FRAME_LENGTH) {
-                            match message_reader.process_bytes::<UsbCodec>(&chunk) {
-                                Ok(messages) => {
-                                    println!("received messages = {:?}", messages);
+                        let messages: Vec<Result<message::SerialMessage, _>> =
+                            message::try_decode_messages(&mut serial_buf[0..t]).collect();
+                        for message in messages {
+                            match message {
+                                Ok(message) => {
+                                    println!("received messages = {:?}", message);
                                 }
-                                Err(e) => eprintln!("Error while parsing messages = {:?}", e),
+                                Err(e) => eprintln!("Error while decoding message = {:?}", e),
                             }
                         }
                     }
